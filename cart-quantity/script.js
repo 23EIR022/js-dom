d1d4f42918eb791cd4c3b1aa05b3f6de3627a141
23EIR022 <dhanusripgm@gmail.com>
2025-06-20 14:00:38 +0530

    complete cart quantity

---
+++ cart-quantity/script.js
@@ -0,0 +1,21 @@
+//console.log("HEllo");
+//cartQuantity=0;
+let cartQuantity= Number(localStorage.getItem("cartQuantity"));
+// if(!cartQuantity) {
+//     localStorage.setItem("cartQuantity",0);
+//     cartQuantity=0;
+// }
+   cartQuantity = cartQuantity || 0;
+
+function updateQuantity(value) {
+    cartQuantity=cartQuantity+value;
+    localStorage.setItem("cartQuantity",cartQuantity);
+}
+function displayQuantity() {
+    console.log(`cart: ${cartQuantity}`);
+}
+function resetCart() {
+    cartQuantity=0;
+    localStorage.setItem("cartQuantity",cartQuantity);
+    console.log('The cart has been reset.');
+}
