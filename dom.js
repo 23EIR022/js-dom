6bbca9f307f642762b4b11eda9ecf48fc4a34c96
23EIR022 <dhanusripgm@gmail.com>
2025-06-20 15:31:18 +0530

    complete hibutton

---
+++ dom.js
@@ -0,0 +1,12 @@
+const para = document.getElementById("msg");
+console.log(para.innerText);
+
+para.innerText = "This is set via JavaScript";
+
+// const hibutton = document.querySelector("#js-button")
+const hiButton=document.querySelector(".js-button");
+
+// event listeners
+hiButton.addEventListener("click",() => {
+    console.log("Hi");
+});
