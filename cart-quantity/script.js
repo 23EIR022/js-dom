79bc25bd09d2655d53cc98cde6f1b7c346d48111
23EIR022 <dhanusripgm@gmail.com>
2025-06-20 15:28:58 +0530

    car quantity-buttons

--- cart-quantity/script.js
+++ cart-quantity/script.js
@@ -1,21 +1,46 @@
 //console.log("HEllo");
 //cartQuantity=0;
 let cartQuantity= Number(localStorage.getItem("cartQuantity"));
-// if(!cartQuantity) {
-//     localStorage.setItem("cartQuantity",0);
-//     cartQuantity=0;
+//  if(!cartQuantity) {
+//    localStorage.setItem("cartQuantity",0);
+//    cartQuantity=0;
 // }
    cartQuantity = cartQuantity || 0;
 
+   const displayButton=document.getElementById("display-button");
+   const plusOneButton=document.getElementById("plus-one");
+   const plusTwoButton=document.getElementById("plus-two");
+   const plusThreeButton=document.getElementById("plus-three");
+   const resetButton=document.getElementById("reset");
+
+   displayButton.addEventListener("click",() => {
+    displayQuantity();
+   });
+    plusOneButton.addEventListener("click",() => {
+    updateQuantity(1);
+    displayQuantity();
+   });
+    plusTwoButton.addEventListener("click",() => {
+    updateQuantity(2);
+    displayQuantity();
+   });
+    plusThreeButton.addEventListener("click",() => {
+    updateQuantity(3);
+    displayQuantity();
+   });
+   resetButton.addEventListener("click",() => {
+    resetCart();
+    displayQuantity();
+   });
 function updateQuantity(value) {
-    cartQuantity=cartQuantity+value;
+   cartQuantity=cartQuantity+value;
     localStorage.setItem("cartQuantity",cartQuantity);
 }
 function displayQuantity() {
-    console.log(`cart: ${cartQuantity}`);
+   console.log(`cart: ${cartQuantity}`);
 }
-function resetCart() {
+ function resetCart() {
     cartQuantity=0;
-    localStorage.setItem("cartQuantity",cartQuantity);
+localStorage.setItem("cartQuantity",cartQuantity);
     console.log('The cart has been reset.');
-}
+ }
