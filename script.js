04cbf3ab0cf3431bc85ab27d7fb1abe003fe5ab5
23EIR022 <dhanusripgm@gmail.com>
2025-06-20 10:50:03 +0530

    omplete JSON

---
+++ script.js
@@ -0,0 +1,33 @@
+let newObject = {
+    name: "Dhanu",
+    display: function() {
+        console.log("Hello");
+    }
+}
+newObject.display();
+
+// types of console Methods used:
+  console.log()
+  console.warn()
+  console.error()
+  console.table()
+   console.time()
+    // console object Methods
+  console.log("Hello JS!");
+  console.warn("This is a warning");
+  console.error("Something went wrong");
+  console.table([{name: "alice"}, {name:"bob"}]);
+  console.time("loop");
+  for (let i=0;i<1000000;i++){ }
+  console.timeEnd("loop");
+
+  // eval() function - don't use in live projects
+  const x =10;
+  const y=20;
+  const result = eval("x+y");
+  console.log(result);
+
+  // strict Mode
+  "use strict";
+  let a = 10;
+  b = 20;
